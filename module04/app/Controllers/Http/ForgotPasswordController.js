8ff3b3b653b790bfa886593a9dbac88af949c258
Pedro Fagundes <pedrofagundesb@gmail.com>
2019-02-21 21:52:25 -0300

    Finishing user related features: Create user, forgot password

---
+++ module04/app/Controllers/Http/ForgotPasswordController.js
@@ -0,0 +1,69 @@
+'use strict'
+
+const moment = require('moment')
+const crypto = require('crypto')
+const User = use('App/Models/User')
+const Mail = use('Mail')
+
+class ForgotPasswordController {
+  async store ({ request, response }) {
+    const email = request.input('email')
+
+    try {
+      const user = await User.findByOrFail('email', email)
+      user.token = crypto.randomBytes(10).toString('hex')
+      user.token_created_at = new Date()
+
+      await user.save()
+
+      await Mail.send(
+        ['emails.forgot_password'],
+        {
+          email,
+          token: user.token,
+          link: `${request.input('redirect_url')}?token=${user.token}`
+        },
+        message => {
+          message
+            .to(user.email)
+            .from('pedrofagundesb@gmail.com', 'Pedro Henrique')
+            .subject('Recuperação de senha')
+        }
+      )
+    } catch (error) {
+      return response
+        .status(error.status)
+        .send({ error: { message: 'Usuário com este e-mail não encontrado' } })
+    }
+  }
+
+  async update ({ request, response }) {
+    try {
+      const { token, password } = request.all()
+
+      const user = await User.findByOrFail('token', token)
+
+      const tokenExpired = moment()
+        .subtract('1', 'days')
+        .isAfter(user.token_created_at)
+
+      if (tokenExpired) {
+        return response
+          .status(401)
+          .send({ error: { message: 'O token de recuperação está expirado' } })
+      }
+
+      user.token = null
+      user.token_created_at = null
+      user.password = password
+
+      await user.save()
+    } catch (error) {
+      return response.status(error.status).send({
+        error: { message: 'Houve um erro ao tentar resetar sua senha.' }
+      })
+    }
+  }
+}
+
+module.exports = ForgotPasswordController
